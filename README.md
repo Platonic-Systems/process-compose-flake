9b37aa3e7ea5125bc83852e8b21c37594774a546
Marijan Petričević <marijan.petricevic94@gmail.com>
2022-12-08 15:28:28 -0500

    initial implementation

---
+++ README.md
@@ -0,0 +1,45 @@
+# process-compose-flake
+Based on [process-compose](https://github.com/F1bonacc1/process-compose)
+
+## Usage
+Let's say you want to have a devShell which makes a command `watch-server` available, that you can use to spin up your projects `backend-server`, `frontend-server` and `proxy-server`.
+
+To achieve this using `process-compose-flake` you can simply add the following code to the `perSystem` function in your `flake-parts` flake.
+```
+process-compose.configs = {
+  watch-server.processes = {
+    backend-server.command = "${self'.apps.backend-server.program} --port 9000";
+    frontend-server.command = "${self'.apps.frontend-server.program} --port 9001";
+    proxy-server.command =
+      let
+        proxyConfig = pkgs.writeTextFile {
+          name = "proxy.conf";
+          text = ''
+            ...
+          '';
+        };
+      in
+      "${self'.apps.proxy-server.program} -c ${proxyConfig} -p 8000";
+  };
+};
+```
+
+`process-compose-flake` will generate the following two outputs for you:
+  - `apps.${system}.watch-server`
+  - `packages.${system}.watch-server`
+
+Using the `apps` output you can spin up the processes by running `nix run .#watch-server`.
+
+The `package` output in turn can be used to make the `watch-server` command available in your devShell:
+```
+devShells = {
+  default = pkgs.mkShell {
+    name = "my-shell";
+    buildInputs = [
+      self'.packages.watch-server
+    ];
+  };
+};
+```
+
+You can enter your devShell by running `nix develop` and run `watch-server` to run your processes.
---
+++ flake-module.nix
@@ -0,0 +1,83 @@
+{ self, lib, flake-parts-lib, ... }:
+let
+  inherit (flake-parts-lib)
+    mkPerSystemOption;
+  inherit (lib)
+    mdDoc
+    mkOption
+    types;
+in
+{
+  options = {
+    perSystem = mkPerSystemOption
+      ({ config, self', inputs', pkgs, system, ... }: {
+        options.process-compose = mkOption {
+          description = mdDoc ''
+            process-compose-flake: creates process-compose executables
+            from your Nix attribute sets.
+          '';
+          type = types.submodule {
+            options = {
+              package = mkOption {
+                type = types.package;
+                default = pkgs.process-compose;
+              };
+              configs = mkOption {
+                type = types.attrsOf types.attrs;
+                default = { };
+                example = {
+                  # apps.${system}.watch-server and packages.${system}.watch-server become available
+                  # execute `nix run .#watch-server` or incude it as a buildInput to your devShell
+                  watch-server = {
+                    processes = {
+                      backend = "${pkgs.simple-http-server}";
+                      frontend = "${pkgs.simple-http-server}";
+                    };
+                  };
+                };
+                description = mdDoc ''
+                  For each attribute `x = process-compose config` a flake app and package `x` is added to the flake.
+                  Which runs process-compose with the declared config.
+                '';
+              };
+            };
+          };
+        };
+      });
+  };
+  config = {
+    perSystem = { config, self', inputs', pkgs, ... }:
+      let
+        toYAMLFile =
+          attrs:
+          pkgs.runCommand "toYamlFile" { buildInputs = [ pkgs.yq-go ]; } ''
+            yq -P '.' ${pkgs.writeTextFile { name = "tmp.json"; text = (builtins.toJSON attrs); }} > $out
+          '';
+        packages = pkgs.lib.mapAttrs'
+          (name: processComposeConfig: {
+            inherit name;
+            value = pkgs.writeShellApplication {
+              inherit name;
+              runtimeInputs = [ config.process-compose.package ];
+              text = ''
+                process-compose -f ${toYAMLFile processComposeConfig} "$@"
+              '';
+            };
+          })
+          config.process-compose.configs;
+      in
+      {
+        inherit packages;
+        apps = pkgs.lib.mapAttrs'
+          (name: _: {
+            inherit name;
+            value = {
+              type = "app";
+              program = packages.${name};
+            };
+          })
+          config.process-compose.configs;
+      };
+  };
+}
+
