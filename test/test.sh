d6723f2de324522d5dc900bae42f5a506d3f1f78
Sridhar Ratnakumar <srid@srid.ca>
2023-06-09 14:13:05 -0400

    Allow test.sh to be run from any dir

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
+cd "$(dirname "$0")"
 rm -f ./data.sqlite
 nix run -L --override-input process-compose-flake .. . -- -t=false
 [[ $(cat result.txt) == "Hello" ]] && echo "Test passed" || (echo "Test failed" && exit 1)
