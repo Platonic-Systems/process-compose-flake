89bc87058dd6ce327bc62e6287af89b4ef54613c
Sridhar Ratnakumar <srid@srid.ca>
2023-06-12 17:44:28 -0400

    refactor: namespace config.hello

--- nix/services/hello.nix
+++ nix/services/hello.nix
@@ -8,17 +8,19 @@
       default = "hello";
       description = "Process name";
     };
-    package = lib.mkPackageOption pkgs "hello" {};
+    package = lib.mkPackageOption pkgs "hello" { };
     greeting = lib.mkOption {
       type = lib.types.str;
       default = "Hello";
       description = "The greeting to use";
     };
   };
-  config.settings = lib.mkIf config.hello.enable {
-    processes.${config.hello.name}.command = ''
-      set -x
-      ${lib.getExe config.hello.package} -g "${config.hello.greeting}"
-    '';
-  };
+  config =
+    let cfg = config.hello;
+    in lib.mkIf cfg.enable {
+      settings.processes.${cfg.name}.command = ''
+        set -x
+        ${lib.getExe cfg.package} -g "${cfg.greeting}"
+      '';
+    };
 }
