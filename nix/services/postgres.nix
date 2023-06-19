f09e735f06537e0f784fdb8c7764af1901679be1
Sridhar Ratnakumar <srid@srid.ca>
2023-06-19 11:01:04 -0400

    Flesh out postgres service, and add a test

--- nix/services/postgres.nix
+++ nix/services/postgres.nix
@@ -1,5 +1,8 @@
 { pkgs, lib, config, ... }:
 
+let
+  inherit (lib) types;
+in
 {
   options.services.postgres = lib.mkOption {
     description = ''
@@ -20,23 +23,209 @@
           default = "./data/${config.name}";
           description = "Postgres data directory";
         };
+        listen_addresses = lib.mkOption {
+          type = lib.types.str;
+          description = "Listen address";
+          default = "";
+          example = "127.0.0.1";
+        };
+
+        port = lib.mkOption {
+          type = lib.types.port;
+          default = 5432;
+          description = ''
+            The TCP port to accept connections.
+          '';
+        };
+
+        createDatabase = lib.mkOption {
+          type = types.bool;
+          default = true;
+          description = ''
+            Create a database named like current user on startup. Only applies when initialDatabases is an empty list.
+          '';
+        };
+
+        initdbArgs = lib.mkOption {
+          type = types.listOf types.lines;
+          default = [ "--locale=C" "--encoding=UTF8" ];
+          example = [ "--data-checksums" "--allow-group-access" ];
+          description = ''
+            Additional arguments passed to `initdb` during data dir
+            initialisation.
+          '';
+        };
+
+        settings = lib.mkOption {
+          type = with lib.types; attrsOf (oneOf [ bool float int str ]);
+          default = { };
+          description = ''
+            PostgreSQL configuration. Refer to
+            <https://www.postgresql.org/docs/11/config-setting.html#CONFIG-SETTING-CONFIGURATION-FILE>
+            for an overview of `postgresql.conf`.
+
+            String values will automatically be enclosed in single quotes. Single quotes will be
+            escaped with two single quotes as described by the upstream documentation linked above.
+          '';
+          default = {
+            listen_addresses = config.listen_addresses;
+            port = config.port;
+            unix_socket_directories = lib.mkDefault config.dataDir;
+          };
+          example = lib.literalExpression ''
+            {
+              log_connections = true;
+              log_statement = "all";
+              logging_collector = true
+              log_disconnections = true
+              log_destination = lib.mkForce "syslog";
+            }
+          '';
+        };
+
+        initialDatabases = lib.mkOption {
+          type = types.listOf (types.submodule {
+            options = {
+              name = lib.mkOption {
+                type = types.str;
+                description = ''
+                  The name of the database to create.
+                '';
+              };
+              schema = lib.mkOption {
+                type = types.nullOr types.path;
+                default = null;
+                description = ''
+                  The initial schema of the database; if null (the default),
+                  an empty database is created.
+                '';
+              };
+            };
+          });
+          default = [ ];
+          description = ''
+            List of database names and their initial schemas that should be used to create databases on the first startup
+            of Postgres. The schema attribute is optional: If not specified, an empty database is created.
+          '';
+          example = lib.literalExpression ''
+            [
+              {
+                name = "foodatabase";
+                schema = ./foodatabase.sql;
+              }
+              { name = "bardatabase"; }
+            ]
+          '';
+        };
+
+        initialScript = lib.mkOption {
+          type = types.nullOr types.str;
+          default = null;
+          description = ''
+            Initial SQL commands to run during database initialization. This can be multiple
+            SQL expressions separated by a semi-colon.
+          '';
+          example = lib.literalExpression ''
+            CREATE USER postgres SUPERUSER;
+            CREATE USER bar;
+          '';
+        };
       };
     });
   };
   config = let cfg = config.services.postgres; in lib.mkIf cfg.enable {
     # TODO: Bring over https://github.com/cachix/devenv/blob/main/src/modules/services/postgres.nix
     settings.processes = {
-      "${cfg.name}-init".command = ''
-        mkdir -p "${cfg.dataDir}"
-        export PATH="${cfg.package}"/bin:$PATH
-        initdb -D "${cfg.dataDir}"
-      '';
+      "${cfg.name}-init".command =
+        let
+          toStr = value:
+            if true == value then
+              "yes"
+            else if false == value then
+              "no"
+            else if lib.isString value then
+              "'${lib.replaceStrings [ "'" ] [ "''" ] value}'"
+            else
+              toString value;
+          configFile = pkgs.writeText "postgresql.conf" (lib.concatStringsSep "\n"
+            (lib.mapAttrsToList (n: v: "${n} = ${toStr v}") cfg.settings));
+
+          setupInitialDatabases =
+            if cfg.initialDatabases != [ ] then
+              (lib.concatMapStrings
+                (database: ''
+                  echo "Checking presence of database: ${database.name}"
+                  # Create initial databases
+                  dbAlreadyExists="$(
+                    echo "SELECT 1 as exists FROM pg_database WHERE datname = '${database.name}';" | \
+                    postgres --single -E postgres | \
+                    ${pkgs.gnugrep}/bin/grep -c 'exists = "1"' || true
+                  )"
+                  echo $dbAlreadyExists
+                  if [ 1 -ne "$dbAlreadyExists" ]; then
+                    echo "Creating database: ${database.name}"
+                    echo 'create database "${database.name}";' | postgres --single -E postgres
+
+
+                    ${lib.optionalString (database.schema != null) ''
+                    echo "Applying database schema on ${database.name}"
+                    if [ -f "${database.schema}" ]
+                    then
+                      echo "Running file ${database.schema}"
+                      ${pkgs.gawk}/bin/awk 'NF' "${database.schema}" | postgres --single -j -E ${database.name}
+                    elif [ -d "${database.schema}" ]
+                    then
+                      # Read sql files in version order. Apply one file
+                      # at a time to handle files where the last statement
+                      # doesn't end in a ;.
+                      ls -1v "${database.schema}"/*.sql | while read f ; do
+                        echo "Applying sql file: $f"
+                        ${pkgs.gawk}/bin/awk 'NF' "$f" | postgres --single -j -E ${database.name}
+                      done
+                    else
+                      echo "ERROR: Could not determine how to apply schema with ${database.schema}"
+                      exit 1
+                    fi
+                    ''}
+                  fi
+                '')
+                cfg.initialDatabases)
+            else
+              lib.optionalString cfg.createDatabase ''
+                echo "CREATE DATABASE ''${USER:-$(id -nu)};" | postgres --single -E postgres '';
+          runInitialScript =
+            if cfg.initialScript != null then
+              ''
+                echo "${cfg.initialScript}" | postgres --single -E postgres
+              ''
+            else
+              "";
+        in
+        ''
+          export PATH="${cfg.package}"/bin:$PATH
+          export PGDATA="${cfg.dataDir}"
+          if [[ ! -d "$PGDATA" ]]; then
+            initdb ${lib.concatStringsSep " " cfg.initdbArgs}
+            ${setupInitialDatabases}
+
+            ${runInitialScript}
+          fi
+
+          # Setup config
+          cp ${configFile} "$PGDATA/postgresql.conf"
+        '';
       ${cfg.name} = {
         command = ''
+          set -x
           export PATH="${cfg.package}"/bin:$PATH
-          postgres -D ${cfg.dataDir}
+          export LOCKDIR="/tmp"
+          postgres -k $LOCKDIR -i -h 127.0.0.1 -D ${cfg.dataDir} 
         '';
         depends_on."${cfg.name}-init".condition = "process_completed_successfully";
+        # SIGINT (= 2) for faster shutdown: https://www.postgresql.org/docs/current/server-shutdown.html
+        shutdown.signal = 2;
+        # https://github.com/F1bonacc1/process-compose#-auto-restart-if-not-healthy
+        availability.restart = "on_failure";
       };
     };
   };
--- test.sh
+++ test.sh
@@ -2,11 +2,18 @@ set -euxo pipefail
 
 cd "$(dirname "$0")"
 
-cd ./example
 # First, build the example.
+pushd ./example
 nix build -L --no-link --print-out-paths --override-input process-compose-flake ..
 
 # On NixOS, run the VM tests to test runtime behaviour
 if command -v nixos-rebuild &> /dev/null; then
+  # TODO: Can we run all these tests in the same VM?
+
+  # example
   nix flake check -L --override-input process-compose-flake ..
+
+  # service tests
+  pushd ../nix/services/test 
+  nix flake check -L --override-input process-compose-flake ../../..
 fi
