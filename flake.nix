dd1dff00700deb2bc4afa44e7082c0176c095fd9
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-06-19 13:13:25 -0400

    Add devenv-like services (starting with postgresql) (#17)
    - Add hello service
    - Add postgresql service
    - Add tests for service

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   outputs = _: {
     flakeModule = ./nix/flake-module.nix;
 
+    processComposeModules = {
+      services = ./nix/services;
+    };
+
     templates.default = {
       description = "Example flake using process-compose-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
