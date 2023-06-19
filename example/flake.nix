8b95be48b4e8f2c2d8c1970ef3b04e431a43042c
Sridhar Ratnakumar <srid@srid.ca>
2023-06-19 13:05:28 -0400

    revert example flake

--- example/flake.nix
+++ example/flake.nix
@@ -18,19 +18,6 @@
       perSystem = { self', pkgs, lib, ... }: {
         # This adds a `self.packages.default`
         process-compose."default" = let port = 8213; in {
-          # process-compose-flake exports some handy services. You may use them
-          # as follows: Import them in `imports`, and set the corresponding
-          # option.
-          imports = [
-            inputs.process-compose-flake.processComposeModules.services
-          ];
-          services.hello = {
-            enable = true;
-            name = "welcome";
-            greeting = "Welcome to sqlite-web demo";
-          };
-
-          # Raw settings (process-compose.yaml) can be specified here.
           settings = {
             environment = {
               DATAFILE = "data.sqlite";
