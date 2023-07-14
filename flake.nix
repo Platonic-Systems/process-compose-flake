2aa84559e0ff8fd48396116a147340f8c699ecd8
Sridhar Ratnakumar <srid@srid.ca>
2023-07-14 14:39:19 -0400

    Add nixci configuration

--- flake.nix
+++ flake.nix
@@ -6,5 +6,12 @@
       description = "Example flake using process-compose-flake";
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
     };
+
+    nixci = let overrideInputs = { process-compose-flake = ./.; }; in {
+      example = {
+        inherit overrideInputs;
+        dir = "example";
+      };
+    };
   };
 }
