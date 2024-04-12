bd94a58388163c92737ced3705378228f2d412b5
Sridhar Ratnakumar <srid@srid.ca>
2024-04-12 18:41:02 +1000

    chore(nix): Add dev sub-flake

---
+++ dev/flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+      perSystem = { pkgs, lib, config, ... }: {
+        treefmt = {
+          projectRoot = inputs.process-compose-flake;
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.treefmt.build.devShell
+          ];
+        };
+      };
+    };
+}
