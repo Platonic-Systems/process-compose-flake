c0fb6ab61ff6d3f883a9239b98477df2a845abee
Patrik Stutz <patrik.stutz@gmail.com>
2024-10-14 21:02:32 +0200

    change order of attributes

--- nix/process-compose/default.nix
+++ nix/process-compose/default.nix
@@ -57,7 +57,7 @@ in
             fi
 
             set -x
-            process-compose "''${params[@]}" "$@"
+            process-compose "$@" "''${params[@]}"
             set +x
 
             ${postHook}
