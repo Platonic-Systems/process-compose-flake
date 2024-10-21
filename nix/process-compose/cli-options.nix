7221333ef9d9d9d7239f0376f9b803b02b53d4e3
Sridhar Ratnakumar <srid@srid.ca>
2024-10-21 10:09:47 -0400

    Use types.submodule so we can add description to these options

--- nix/process-compose/cli-options.nix
+++ nix/process-compose/cli-options.nix
@@ -5,41 +5,47 @@ in
 {
   options = {
     cli = {
-      options = {
-        log-file = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = "Pass --log-file to process-compose";
-        };
-        no-server = mkOption {
-          type = types.bool;
-          default = false;
-          description = "Pass --no-server to process-compose";
-        };
-        ordered-shutdown = mkOption {
-          type = types.bool;
-          default = false;
-          description = "Pass --ordered-shutdown to process-compose";
-        };
-        port = mkOption {
-          type = types.nullOr types.int;
-          default = null;
-          description = "Pass --port to process-compose";
-        };
-        read-only = mkOption {
-          type = types.bool;
-          default = false;
-          description = "Pass --read-only to process-compose";
-        };
-        unix-socket = mkOption {
-          type = types.nullOr types.str;
-          default = null;
-          description = "Pass --unix-socket to process-compose";
-        };
-        use-uds = mkOption {
-          type = types.bool;
-          default = false;
-          description = "Pass --use-uds to process-compose";
+      options = mkOption {
+        description = "CLI options to pass to process-compose binary";
+        default = { };
+        type = types.submodule {
+          options = {
+            log-file = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = "Pass --log-file to process-compose";
+            };
+            no-server = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Pass --no-server to process-compose";
+            };
+            ordered-shutdown = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Pass --ordered-shutdown to process-compose";
+            };
+            port = mkOption {
+              type = types.nullOr types.int;
+              default = null;
+              description = "Pass --port to process-compose";
+            };
+            read-only = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Pass --read-only to process-compose";
+            };
+            unix-socket = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = "Pass --unix-socket to process-compose";
+            };
+            use-uds = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Pass --use-uds to process-compose";
+            };
+          };
         };
       };
 
@@ -48,6 +54,7 @@ in
         # TODO: We should refactor this to generically iterate on options and produce the CLI automatically using naming conventions and types.
         options = lib.mkOption {
           type = types.str;
+          description = "The final CLI arguments we will pass to process-compose binary.";
           default = let o = config.cli.options; in lib.escapeShellArgs (
             (lib.optionals (o.log-file != null) [ "--log-file" o.log-file ])
             ++ (lib.optionals o.no-server [ "--no-server" ])
