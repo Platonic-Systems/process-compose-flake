d04a5862304788c2396b17c942aa1f0dc2dc1500
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-11-08 10:24:05 -0500

    Revert "add detached CLI option (#87)"
    This reverts commit b590e3956e844391cb549fe3d529e5bde4271130.

--- nix/process-compose/cli.nix
+++ nix/process-compose/cli.nix
@@ -33,11 +33,6 @@ in
         default = { };
         type = types.submodule {
           options = {
-            detached = mkOption {
-              type = types.bool;
-              default = false;
-              description = "Pass --detached to process-compose";
-            };
             log-file = mkOption {
               type = types.nullOr types.str;
               default = null;
@@ -85,8 +80,7 @@ in
           readOnly = true;
           description = "The final CLI arguments we will pass to process-compose binary.";
           default = let o = config.cli.options; in lib.escapeShellArgs (
-            (lib.optionals o.detached [ "--detached" ])
-            ++ (lib.optionals (o.log-file != null) [ "--log-file" o.log-file ])
+            (lib.optionals (o.log-file != null) [ "--log-file" o.log-file ])
             ++ (lib.optionals o.no-server [ "--no-server" ])
             ++ (lib.optionals o.ordered-shutdown [ "--ordered-shutdown" ])
             ++ (lib.optionals (o.port != null) [ "--port" "${builtins.toString o.port}" ])
