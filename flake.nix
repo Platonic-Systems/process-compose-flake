5d4c63640b62f5f79e605e7a5d8de3074fcb6c14
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2025-06-09 05:20:33 -0400

    ci: Disable nix-ci.com

--- flake.nix
+++ flake.nix
@@ -9,6 +9,9 @@
       path = builtins.path { path = ./example; filter = path: _: baseNameOf path == "flake.nix"; };
     };
 
+    # We don't use nix-ci.com (#96)
+    nix-ci.enable = false
+
     # https://github.com/srid/nixci
     nixci.default = let overrideInputs = { process-compose-flake = ./.; }; in {
       example = {
